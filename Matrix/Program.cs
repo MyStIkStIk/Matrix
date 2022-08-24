473735e7b915512f02b2391d945a11aaaf83c9b9
Vladyslav Liashuk <mystikhawk333@gmail.com>
2022-08-24 21:17:09 +0300

    переработано

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,26 +10,38 @@ namespace Matrix
 {
     internal class Program
     {
-        static void Matrix(object column)
+
+        public class Line
         {
-            Random random = new Random();
-            int[] matrixMass = new int[random.Next(5, 11)];
-            for (int i = 0; i < matrixMass.Length; i++)
+            readonly int column;
+            readonly byte[] lockline;
+            public Line(int column, ref byte[] lockLine)
             {
-                if (i == 0)
-                    matrixMass[i] = 1;
-                else if (i == 1)
-                    matrixMass[i] = 2;
-                else
-                    matrixMass[i] = 3;
+                Thread thread = new Thread(Matrix);
+                this.column = column;
+                this.lockline = lockLine;
+                thread.Start();
             }
-            int[] rowMass = new int[Console.WindowHeight + matrixMass.Length];
-            while (true)
+            private void Matrix()
             {
+                Random random = new Random();
+                int[] matrixMass = new int[random.Next(5, 11)];
+                for (int i = 0; i < matrixMass.Length; i++)
+                {
+                    if (i == 0)
+                        matrixMass[i] = 1;
+                    else if (i == 1)
+                        matrixMass[i] = 2;
+                    else
+                        matrixMass[i] = 3;
+                }
+                int[] rowMass = new int[Console.WindowHeight + matrixMass.Length];
+                this.lockline[column]++;
                 for (int i = 0; i < rowMass.Length; i++)
                 {
                     lock (rowMass)
                     {
+                        Thread.Sleep(random.Next(300, 601));
                         for (int k = 0; k < rowMass.Length; k++)
                         {
                             if (k > i - matrixMass.Length)
@@ -41,14 +54,11 @@ namespace Matrix
                             else
                                 rowMass[k] = 0;
                         }
-                    }
-                    lock (rowMass)
-                    {
                         for (int j = 0; j < rowMass.Length; j++)
                         {
                             if (j < Console.WindowHeight)
                             {
-                                Console.SetCursorPosition((int)column, j);
+                                Console.SetCursorPosition(column, j);
                                 if (rowMass[j] == 0)
                                 {
                                     Console.Write(" ");
@@ -70,19 +80,25 @@ namespace Matrix
                                 }
                             }
                         }
-                        Thread.Sleep(random.Next(150));
                     }
+                    this.lockline[(int)column]--;
                 }
             }
         }
         static void Main(string[] args)
         {
-            Console.SetWindowSize(100, 30);
-            Console.SetBufferSize(100, 30);
-
-            for (int i = 0; i < Console.WindowWidth; i++)
+            Console.CursorVisible = false;
+            byte[] lockLine = new byte[Console.WindowWidth];
+            Random rand = new Random();
+            while (true)
             {
-                new Thread(Matrix).Start(i);
+                Thread.Sleep(30);
+                int i = rand.Next(0, lockLine.Length);
+                if (lockLine[i] == 1)
+                {
+                    continue;
+                }
+                new Line(i, ref lockLine);
             }
         }
     }
